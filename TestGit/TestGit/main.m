68d14e246e41d742489d81711141b821695ae130
xiezw <xiezw@g-bits.com>
2019-05-23 16:46:24 +0800

    添加TestGit工程

---
+++ TestGit/TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by xiezw on 2019/5/23.
+//  Copyright © 2019 xiezw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
